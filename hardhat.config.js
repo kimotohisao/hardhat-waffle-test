014d041770ba141714b0175c8f9379d8bcd39658
Kimoto <kimotohisao17@gmail.com>
2025-01-09 19:25:14 +0900

    secode commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require('hardhat-dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+
+const { PRIVATE_KEY, INFURA_PROJECT_ID } = process.env;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
